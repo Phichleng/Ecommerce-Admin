1a4a4c86cb58bee412bc9a3a625a772ac0cf178d
phichleng <phichlengleav@gmail.com>
2023-08-17 20:42:33 +0700

    admin-das

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,44 @@
+import clientPromise from "@/lib/mongodb";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth, { getServerSession } from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"
+
+
+
+
+const adminEmails = ['pinklaing11@gmail.com'];
+
+export const authOptions = {
+    // Configure one or more authentication provider
+
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_SECRET,
+        }),
+
+    ],
+    adapter: MongoDBAdapter(clientPromise),
+    callbacks: {
+        session: ({session,token,user}) => {
+            if (adminEmails.includes(session?.user?.email)) {
+                return session;
+            } else {
+                return false;
+            }
+            
+        },
+    },
+}
+
+export default NextAuth(authOptions);
+
+
+export async function isAdminRequest(req,res) {
+    const session = await getServerSession(req,res,authOptions);
+    if (!adminEmails.includes(session?.user?.email)) {
+        res.status(401);
+        res.end();
+        throw 'not an admin';
+    }
+}
